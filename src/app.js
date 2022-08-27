c6830e6a7b88785584a6716bcf03d38b221c297c
Ilya <Ilya.Lelkov@mail.ru>
2022-08-27 12:44:58 +1000

    fersionStart

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
 import './styles.css'
+import {ContextMenu} from "./menu";
 
+const contex = new ContextMenu()
+contex.open()
+contex.close()
+contex.add()
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,27 @@
 import {Menu} from './core/menu'
 
+const menuElementHTML = document.querySelector('.menu')
+
 export class ContextMenu extends Menu {
+    open() {
+        document.addEventListener('contextmenu', event => {
+            event.preventDefault()
+            menuElementHTML.classList.add('open')
+            menuElementHTML.style.top = `${event.clientY}px`
+            menuElementHTML.style.left = `${event.clientX}px`
+        })
+    }
+
+    close() {
+        document.addEventListener('click', event => {
+            if (event.button !== 2) {
+                menuElementHTML.classList.remove('open')
+            }
+        })
+
+        menuElementHTML.addEventListener('click', event => {
+            event.stopPropagation()
+        })
+    }
 
 }
