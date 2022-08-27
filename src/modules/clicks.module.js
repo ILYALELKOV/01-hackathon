240d1a39f7303153d5cd1dc6e2cf78074d032e3e
Ilya <Ilya.Lelkov@mail.ru>
2022-08-27 22:13:45 +1000

    newClicks

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -4,26 +4,54 @@ import {getRandomColor} from "../utils";
 
 export class ClicksModule extends Module {
     trigger() {
-        let count1 = 0
-        let count2 = 0
+        let click = 0
+        let dblclick = 0
 
-        document.body.addEventListener('click', event => {
-            count1 += 1
-            console.log(count1)
+        document.body.addEventListener('click', () => {
+            click += 1
         })
 
-        document.body.addEventListener('dblclick', event => {
-            count2 += 1
-            console.log(count2)
+        document.body.addEventListener('dblclick', () => {
+            dblclick += 1
         })
 
-        // let qwe = 30
-        //
-        // setInterval(() => {
-        //     qwe -= 1
-        //     console.log(qwe)
-        // },1000)
+        menuElementHTML.addEventListener('click', event => {
+            if (event.target.dataset.type === 'clicks') {
+                const elementBlock = document.createElement('div')
 
-        const elementBlock = document.createElement('div')
+                const elementBlockTimer = document.createElement('div')
+                elementBlockTimer.style.width = '400px'
+                elementBlockTimer.style.height = '250px'
+                elementBlockTimer.style.borderRadius = '15px'
+                elementBlockTimer.style.color = '#ffffff'
+                elementBlockTimer.style.textShadow = '2px -2px 1px #00008B'
+                elementBlockTimer.style.fontWeight = '800'
+                elementBlockTimer.style.fontSize = '38px'
+                elementBlockTimer.style.textAlign = 'center'
+                elementBlockTimer.style.lineHeight = '65px'
+                elementBlockTimer.style.margin = '0 auto'
+                elementBlockTimer.style.boxShadow = '0 8px 2px #000'
+                elementBlockTimer.style.background = 'url(../time.jpg)'
+
+                elementBlock.append(elementBlockTimer)
+                document.body.prepend(elementBlock)
+
+                let timer = 5
+                swal(`Проверим твою реакцию? Сейчас тебе нужно набрать максимальное число кликов. Скорее, время уже идет)`)
+
+                setInterval(() => {
+                    elementBlockTimer.textContent = `Осталось: ${timer}`
+
+                    if (timer === 0) {
+                        elementBlock.remove()
+                        elementBlockTimer.remove()
+                        swal(`Поздравляем! Ваши результаты: одинарный клик = ${click}; двойной клик = ${dblclick}`)
+                        click = 0
+                        dblclick = 0
+                    }
+                    timer -= 1
+                }, 1000)
+            }
+        })
     }
 }
